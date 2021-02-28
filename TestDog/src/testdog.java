6313fe282a55ef4f7ceb37ada291342994388932
HP <HP@DESKTOP-L89B3LR>
2021-02-28 09:13:17 +0300

    test

---
+++ TestDog/src/testdog.java
@@ -0,0 +1,12 @@
+
+public class testdog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Dog dog1=new Dog(beagle, large, orange, six,"Computer Science and Information System College");
+	System.out.println("The breed of the person is:" +dog1.beagle());
+	Systym.out.println("The size of the person is: "+dog1.large());
+	System.out.println("The colour of the person is:"+dog1.orange());
+	
+	}
+}
